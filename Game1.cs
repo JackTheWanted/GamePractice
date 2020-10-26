9236f0c96b1efd3f199f2c9ebe419a89560ed112
Jack Carlson <CarlJack215@GoTVDSB.ca>
2020-10-26 13:25:06 -0400

    g

--- Game1.cs
+++ Game1.cs
@@ -9,13 +9,10 @@ namespace GamePractice
         private GraphicsDeviceManager _graphics;
         private SpriteBatch spriteBatch;
         private Texture2D background;
-        private Texture2D shuttle;
-        private Texture2D earth;
-        private SpriteFont font;
-        private Texture2D arrow;
-        private float angle = 0;
-        private int score = 0;
-        private int x_coord = 450;
+        private Texture2D black;
+        private Texture2D red;
+        private MouseState oldState;
+        int playerTurn = 1;
 
         public Game1()
         {
@@ -27,37 +24,40 @@ namespace GamePractice
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            this.IsMouseVisible = true;
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            background = Content.Load<Texture2D>("Images/stars");
-            shuttle = Content.Load<Texture2D>("Images/shuttle");
-            earth = Content.Load<Texture2D>("Images/earth");
-            font = Content.Load<SpriteFont>("Score");
-            arrow = Content.Load<Texture2D>("arrow");
+            background = Content.Load<Texture2D>("Images/connect4");
+            black = Content.Load<Texture2D>("Images/blackchip");
+            red = Content.Load<Texture2D>("Images/redchip");
+            
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            //score++;
-            angle += 0.01f;
-            KeyboardState state = Keyboard.GetState();
 
-            if (state.IsKeyDown(Keys.Left))
+            MouseState mouseState = Mouse.GetState();
+            MouseState newState = Mouse.GetState();
+
+            
+            int x = mouseState.X;
+            int y = mouseState.Y;
+
+            if (newState.LeftButton == ButtonState.Pressed && oldState.LeftButton == ButtonState.Released)
             {
-                x_coord -= 2;
+                if (x >= 200)
+                {
+                    Mouse.SetPosition(0, 0);
+                }
             }
 
-            if (state.IsKeyDown(Keys.Right))
-            {
-                x_coord += 2;
-            }
+            oldState = newState;
 
             base.Update(gameTime);
         }
@@ -69,17 +69,12 @@ namespace GamePractice
             // TODO: Add your drawing code here
             spriteBatch.Begin();
 
-            spriteBatch.Draw(background, new Rectangle(0, 0, 800, 480), Color.White);
-            spriteBatch.Draw(earth, new Vector2(400, 240), Color.White);
-            spriteBatch.Draw(shuttle, new Vector2(x_coord, 240), Color.White);
-            //spriteBatch.DrawString(font, "Score: " + score, new Vector2(100, 100), Color.White);
 
-            Vector2 location = new Vector2(400, 240);
-            Rectangle sourceRectangle = new Rectangle(0, 0, arrow.Width, arrow.Height);
-            Vector2 origin = new Vector2(arrow.Width / 2, arrow.Height);
-
-
-            spriteBatch.Draw(arrow, location, sourceRectangle, Color.White, angle, origin, 1.0f, SpriteEffects.None, 1);
+            spriteBatch.Draw(black, new Vector2(118, 237), Color.White);
+            spriteBatch.Draw(red, new Vector2(430, 250), Color.White);
+            spriteBatch.Draw(background, new Rectangle(80, 130, 600, 360), Color.White);
+            
+            
 
             spriteBatch.End();
 
