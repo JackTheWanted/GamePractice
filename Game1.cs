9fef8beaeac22d282a75f8fde26559d3f569d44c
Jack Carlson <CarlJack215@GoTVDSB.ca>
2020-11-09 13:33:31 -0500

    Finished Connect 4

--- Game1.cs
+++ Game1.cs
@@ -12,10 +12,10 @@ namespace GamePractice
         private Texture2D black;
         private Texture2D red;
         private MouseState oldState;
-        int i;
-        private int x;
-        private int y;
-        int playerTurn = 1;
+        private SpriteFont font;
+        private SpriteFont winFont;
+        bool gameRunning = true;
+        int playerTurn;
         int[][] board;
 
         public Game1()
@@ -28,8 +28,9 @@ namespace GamePractice
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            playerTurn = 1;
             board = new int[8][];
-            for(i = 0; i < 8; i++) 
+            for(int i = 0; i < 8; i++) 
             {
                 board[i] = new int[7];
             }
@@ -42,6 +43,8 @@ namespace GamePractice
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("Player Turn");
+            winFont = Content.Load<SpriteFont>("Win Font");
             background = Content.Load<Texture2D>("Images/connect4");
             black = Content.Load<Texture2D>("Images/blackchip");
             red = Content.Load<Texture2D>("Images/redchip");
@@ -55,171 +58,277 @@ namespace GamePractice
 
             MouseState mouseState = Mouse.GetState();
             MouseState newState = Mouse.GetState();
+            KeyboardState state = Keyboard.GetState();
 
             int mouseX = mouseState.X;
-            int mouseY = mouseState.Y;
 
-            if (newState.LeftButton == ButtonState.Pressed && oldState.LeftButton == ButtonState.Released)
+            if (gameRunning)
             {
-                if (mouseX >= 138 && mouseX <= 212)
+                if (newState.LeftButton == ButtonState.Pressed && oldState.LeftButton == ButtonState.Released)
                 {
-                    if (playerTurn == 1 && board[0][0] < 8)
+                    if (mouseX >= 138 && mouseX <= 212)
                     {
-                        board[0][0] += 1;
-                        board[0][board[0][0]] = 1;
-                        playerTurn = 2;
+
+                        if (playerTurn == 1 && board[0][0] < 6)
+                        {
+                            board[0][0] += 1;
+                            board[0][board[0][0]] = 1;
+                            DetectWin();
+                            playerTurn = 2;
+                        }
+
+                        else if (playerTurn == 2 && board[0][0] < 6)
+                        {
+                            board[0][0] += 1;
+                            board[0][board[0][0]] = 2;
+                            DetectWin();
+                            playerTurn = 1;
+                        }
+
                     }
 
-                    else if (playerTurn == 2 && board[0][0] < 8)
+                    if (mouseX >= 213 && mouseX <= 287)
                     {
-                        board[0][0] += 1;
-                        board[0][board[0][0]] = 2;
-                        playerTurn = 1;
+                        if (playerTurn == 1 && board[1][0] < 6)
+                        {
+                            board[1][0] += 1;
+                            board[1][board[1][0]] = 1;
+                            DetectWin();
+                            playerTurn = 2;
+                        }
+
+                        else if (playerTurn == 2 && board[1][0] < 6)
+                        {
+                            board[1][0] += 1;
+                            board[1][board[1][0]] = 2;
+                            DetectWin();
+                            playerTurn = 1;
+                        }
                     }
-                    
+
+                    if (mouseX >= 288 && mouseX <= 362)
+                    {
+                        if (playerTurn == 1 && board[2][0] < 6)
+                        {
+                            board[2][0] += 1;
+                            board[2][board[2][0]] = 1;
+                            DetectWin();
+                            playerTurn = 2;
+                        }
+
+                        else if (playerTurn == 2 && board[2][0] < 6)
+                        {
+                            board[2][0] += 1;
+                            board[2][board[2][0]] = 2;
+                            DetectWin();
+                            playerTurn = 1;
+                        }
+                    }
+
+                    if (mouseX >= 363 && mouseX <= 437)
+                    {
+                        if (playerTurn == 1 && board[3][0] < 6)
+                        {
+                            board[3][0] += 1;
+                            board[3][board[3][0]] = 1;
+                            DetectWin();
+                            playerTurn = 2;
+                        }
+
+                        else if (playerTurn == 2 && board[3][0] < 6)
+                        {
+                            board[3][0] += 1;
+                            board[3][board[3][0]] = 2;
+                            DetectWin();
+                            playerTurn = 1;
+                        }
+                    }
+
+                    if (mouseX >= 438 && mouseX <= 512)
+                    {
+                        if (playerTurn == 1 && board[4][0] < 6)
+                        {
+                            board[4][0] += 1;
+                            board[4][board[4][0]] = 1;
+                            DetectWin();
+                            playerTurn = 2;
+                        }
+
+                        else if (playerTurn == 2 && board[4][0] < 6)
+                        {
+                            board[4][0] += 1;
+                            board[4][board[4][0]] = 2;
+                            DetectWin();
+                            playerTurn = 1;
+                        }
+                    }
+
+                    if (mouseX >= 513 && mouseX <= 587)
+                    {
+                        if (playerTurn == 1 && board[5][0] < 6)
+                        {
+                            board[5][0] += 1;
+                            board[5][board[5][0]] = 1;
+                            DetectWin();
+                            playerTurn = 2;
+                        }
+
+                        else if (playerTurn == 2 && board[5][0] < 6)
+                        {
+                            board[5][0] += 1;
+                            board[5][board[5][0]] = 2;
+                            DetectWin();
+                            playerTurn = 1;
+                        }
+                    }
+
+                    if (mouseX >= 588 && mouseX <= 662)
+                    {
+                        if (playerTurn == 1 && board[6][0] < 6)
+                        {
+                            board[6][0] += 1;
+                            board[6][board[6][0]] = 1;
+                            DetectWin();
+                            playerTurn = 2;
+                        }
+
+                        else if (playerTurn == 2 && board[6][0] < 6)
+                        {
+                            board[6][0] += 1;
+                            board[6][board[6][0]] = 2;
+                            DetectWin();
+                            playerTurn = 1;
+                        }
+                    }
                 }
-
-                if (mouseX >= 213 && mouseX <= 287)
-                {
-                    if (playerTurn == 1 && board[1][0] < 8)
-                    {
-                        board[1][0] += 1;
-                        board[1][board[1][0]] = 1;
-                        playerTurn = 2;
-                    }
-
-                    else if (playerTurn == 2 && board[1][0] < 8)
-                    {
-                        board[1][0] += 1;
-                        board[1][board[1][0]] = 2;
-                        playerTurn = 1;
-                    }
-                }
-
-                if (mouseX >= 288 && mouseX <= 362)
-                {
-                    if (playerTurn == 1 && board[2][0] < 8)
-                    {
-                        board[2][0] += 1;
-                        board[2][board[2][0]] = 1;
-                        playerTurn = 2;
-                    }
-
-                    else if (playerTurn == 2 && board[2][0] < 8)
-                    {
-                        board[2][0] += 1;
-                        board[2][board[2][0]] = 2;
-                        playerTurn = 1;
-                    }
-                }
-
-                if (mouseX >= 363 && mouseX <= 437)
-                {
-                    if (playerTurn == 1 && board[3][0] < 8)
-                    {
-                        board[3][0] += 1;
-                        board[3][board[3][0]] = 1;
-                        playerTurn = 2;
-                    }
-
-                    else if (playerTurn == 2 && board[3][0] < 8)
-                    {
-                        board[3][0] += 1;
-                        board[3][board[3][0]] = 2;
-                        playerTurn = 1;
-                    }
-                }
-
-                if (mouseX >= 438 && mouseX <= 512)
-                {
-                    if (playerTurn == 1 && board[4][0] < 8)
-                    {
-                        board[4][0] += 1;
-                        board[4][board[4][0]] = 1;
-                        playerTurn = 2;
-                    }
-
-                    else if (playerTurn == 2 && board[4][0] < 8)
-                    {
-                        board[4][0] += 1;
-                        board[4][board[4][0]] = 2;
-                        playerTurn = 1;
-                    }
-                }
-
-                if (mouseX >= 513 && mouseX <= 587)
-                {
-                    if (playerTurn == 1 && board[5][0] < 8)
-                    {
-                        board[5][0] += 1;
-                        board[5][board[5][0]] = 1;
-                        playerTurn = 2;
-                    }
-
-                    else if (playerTurn == 2 && board[5][0] < 8)
-                    {
-                        board[5][0] += 1;
-                        board[5][board[5][0]] = 2;
-                        playerTurn = 1;
-                    }
-                }
-
-                if (mouseX >= 588 && mouseX <= 662)
-                {
-                    if (playerTurn == 1 && board[6][0] < 8)
-                    {
-                        board[6][0] += 1;
-                        board[6][board[6][0]] = 1;
-                        playerTurn = 2;
-                    }
-
-                    else if (playerTurn == 2 && board[6][0] < 8)
-                    {
-                        board[6][0] += 1;
-                        board[6][board[6][0]] = 2;
-                        playerTurn = 1;
-                    }
-                }
-
-
             }
-
-            oldState = newState;
-                base.Update(gameTime);
-
             
+            //Reset Function
+            if (gameRunning == false && state.IsKeyDown(Keys.Enter))
+            {
+                for (int i = 0; i <= 6; i++)
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        board[i][j] = 0;
+                    }
+                }
+                playerTurn = 1;
+                gameRunning = true;
+            }
+            oldState = newState;
+            
+                base.Update(gameTime);            
         }  
+
+        private void DetectWin()
+        {
+            if (gameRunning)
+            {
+                //Vertical Detection
+                for (int i = 0; i <= 6; i++)
+                {
+                    for (int j = 1; j < 4; j++)
+                    {
+                        if (board[i][j] == playerTurn && board[i][j + 1] == playerTurn && board[i][j + 2] == playerTurn && board[i][j + 3] == playerTurn)
+                        {
+                            gameRunning = false;
+                        }
+                    }
+                }
+
+                //Horizontal Detection
+                for (int j = 1; j <= 4; j++)
+                {
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        if (board[i][j] == playerTurn && board[i + 1][j] == playerTurn && board[i + 2][j] == playerTurn && board[i + 3][j] == playerTurn)
+                        {
+                            gameRunning = false;
+                        }
+
+
+                    }
+                }
+
+                //Diagonal Detection (Right)
+                for (int j = 1; j <= 3; j++)
+                {
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        if (board[i][j] == playerTurn && board[i + 1][j + 1] == playerTurn && board[i + 2][j + 2] == playerTurn && board[i + 3][j + 3] == playerTurn)
+                        {
+                            gameRunning = false;
+                        }
+                    }
+                }
+
+                //Diagonal Detection (Left)
+                for (int j = 1; j <= 3; j++)
+                {
+                    for (int i = 3; i <= 6; i++)
+                    {
+                        if (board[i][j] == playerTurn && board[i - 1][j + 1] == playerTurn && board[i - 2][j + 2] == playerTurn && board[i - 3][j + 3] == playerTurn)
+                        {
+                            gameRunning = false;
+                        }
+                    }
+                }
+            }            
+        }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
             
             spriteBatch.Begin();
-
-            for (x = 0; x < 7; x++)
+            if (gameRunning)
             {
-                for (y = 1; y < 7; y++)
+                if (playerTurn == 1)
                 {
-                    if (board[x][y] == 1)
-                    {
-                        spriteBatch.Draw(red, new Vector2(x * 74 + 138, 466 - (y * 56)), Color.White);
-                    }
+                    spriteBatch.DrawString(font, "Player " + playerTurn + "'s turn", new Vector2(275, 70), Color.Red);
+                }
 
-                    if (board[x][y] == 2)
+                else if (playerTurn == 2)
+                {
+                    spriteBatch.DrawString(font, "Player " + playerTurn + "'s turn", new Vector2(275, 70), Color.Black);
+                }
+
+                for (int x = 0; x < 7; x++)
+                {
+                    for (int y = 1; y < 7; y++)
                     {
-                        spriteBatch.Draw(black, new Vector2(x * 74 + 138, 466 - (y * 56)), Color.White);
+                        if (board[x][y] == 1)
+                        {
+                            spriteBatch.Draw(red, new Vector2(x * 74 + 138, 466 - (y * 56)), Color.White);
+                        }
+
+                        if (board[x][y] == 2)
+                        {
+                            spriteBatch.Draw(black, new Vector2(x * 74 + 138, 466 - (y * 56)), Color.White);
+                        }
                     }
                 }
+                spriteBatch.Draw(background, new Rectangle(100, 130, 600, 360), Color.White);
             }
             
-            spriteBatch.Draw(background, new Rectangle(100, 130, 600, 360), Color.White);
-            
-            
+            if (gameRunning == false)
+            {
+                if (playerTurn == 1)
+                {
+                    GraphicsDevice.Clear(Color.Black);
+                    spriteBatch.DrawString(winFont, "Player " + playerTurn + " wins", new Vector2(250, 90), Color.Red);
+                    spriteBatch.DrawString(winFont, "Press enter to play again", new Vector2(150, 260), Color.Red);
+                }
+                if (playerTurn == 2)
+                {
+                    GraphicsDevice.Clear(Color.Red);
+                    spriteBatch.DrawString(winFont, "Player " + playerTurn + " wins", new Vector2(250, 90), Color.Black);
+                    spriteBatch.DrawString(winFont, "Hit enter to play again", new Vector2(150, 260), Color.Black);
+                }
+            }
 
             spriteBatch.End();
-
             base.Draw(gameTime);
         }
     }
